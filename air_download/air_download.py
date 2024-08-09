3ce519abfdfca0deb31653dcd5f17dc7a74531fd
pierre-nedelec <pierre.nedelec@ucsf.edu>
2024-08-08 23:32:29 -0700

    format with ruff

--- air_download/air_download.py
+++ air_download/air_download.py
@@ -6,127 +6,130 @@ import requests
 from dotenv import dotenv_values
 from urllib.parse import urljoin
 
+
 def parse_args():
     parser = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
 
     parser.add_argument(
-        'url',
-        help='URL for AIR API, e.g. https://air.<domain>.edu/api/'
-        )
+        "url", help="URL for AIR API, e.g. https://air.<domain>.edu/api/"
+    )
+    parser.add_argument("acc", metavar="ACCESSION", help="Accession # to download")
     parser.add_argument(
-        'acc',
-        metavar='ACCESSION',
-        help='Accession # to download'
-        )
+        "-c",
+        "--cred-path",
+        help="Login credentials file. If not present,"
+        " will look for AIR_USERNAME and AIR_PASSWORD"
+        " environment variables.",
+        default=None,
+    )
     parser.add_argument(
-        '-c',
-        '--cred-path',
-        help='Login credentials file. If not present,'
-             ' will look for AIR_USERNAME and AIR_PASSWORD'
-             ' environment variables.', default=None)
+        "-o", "--output", help="Output path", default="./<Accession>.zip"
+    )
+    parser.add_argument("-pf", "--profile", help="Anonymization Profile", default=-1)
+    parser.add_argument("-pj", "--project", help="Project ID", default=-1)
     parser.add_argument(
-        '-o',
-        '--output',
-        help='Output path',
-        default='./<Accession>.zip')
-    parser.add_argument(
-        '-pf',
-        '--profile',
-        help='Anonymization Profile',
-        default=-1)
-    parser.add_argument(
-        '-pj',
-        '--project',
-        help='Project ID',
-        default=-1)
-    parser.add_argument(
-        '-s',
-        '--series_inclusion',
-        help='Comma-separated list of series inclusion patterns',
-        default=None)
-
+        "-s",
+        "--series_inclusion",
+        help="Comma-separated list of series inclusion patterns",
+        default=None,
+    )
 
     arguments = parser.parse_args()
 
-    if arguments.output == './<Accession>.zip':
-        arguments.output = '{acc}.zip'.format(acc=arguments.acc)
+    if arguments.output == "./<Accession>.zip":
+        arguments.output = "{acc}.zip".format(acc=arguments.acc)
 
     return arguments
 
 
 def authenticate(url, cred_path):
     if cred_path:
-        assert os.path.exists(cred_path), f'AIR credential file ({cred_path}) does not exist.'
+        assert os.path.exists(
+            cred_path
+        ), f"AIR credential file ({cred_path}) does not exist."
         envs = dotenv_values(cred_path)
-        userId = envs['AIR_USERNAME']
-        password = envs['AIR_PASSWORD']  
+        userId = envs["AIR_USERNAME"]
+        password = envs["AIR_PASSWORD"]
     else:
-        userId   = os.environ.get('AIR_USERNAME')
-        password = os.environ.get('AIR_PASSWORD')
-        assert (userId and password) is not None, 'AIR credentials not provided.'
-    auth_info = {
-        'userId': userId,
-        'password': password
-    }
+        userId = os.environ.get("AIR_USERNAME")
+        password = os.environ.get("AIR_PASSWORD")
+        assert (userId and password) is not None, "AIR credentials not provided."
+    auth_info = {"userId": userId, "password": password}
 
-    session = requests.post(urljoin(url, 'login'), json = auth_info).json()
-    jwt = session['token']['jwt']
+    session = requests.post(urljoin(url, "login"), json=auth_info).json()
+    jwt = session["token"]["jwt"]
 
-    projects = session['user']['projects']
+    projects = session["user"]["projects"]
     return jwt, projects
 
 
 def get_deid_profiles(url, cred_path):
     jwt, _ = authenticate(url, cred_path)
 
-    header = {'Authorization': 'Bearer ' + jwt}
+    header = {"Authorization": "Bearer " + jwt}
     profiles = requests.post(
-        urljoin(url, 'secure/anonymization/list-profiles'),
-        headers = header,
-        json = {
-            'includeGlobal': True,
-            'includeCustom': True,
-            'includeDefault': False,
-            'includeInactiveCustom': False,
-            'includeInactiveGlobal': False,
-            'includeInactiveShared': False,
-            'includeShared': True
-            }).json()
+        urljoin(url, "secure/anonymization/list-profiles"),
+        headers=header,
+        json={
+            "includeGlobal": True,
+            "includeCustom": True,
+            "includeDefault": False,
+            "includeInactiveCustom": False,
+            "includeInactiveGlobal": False,
+            "includeInactiveShared": False,
+            "includeShared": True,
+        },
+    ).json()
     ret = []
     for profile in profiles:
-        ret.append({k: profile[k] for k in ('id', 'name', 'description')})
+        ret.append({k: profile[k] for k in ("id", "name", "description")})
     return ret
 
 
-def download(url, cred_path, accession, output, project, profile, series_inclusion=None):
+def download(
+    url, cred_path, accession, output, project, profile, series_inclusion=None
+):
     jwt, _ = authenticate(url, cred_path)
-    header = {'Authorization': 'Bearer ' + jwt}
+    header = {"Authorization": "Bearer " + jwt}
 
-    # Search for study by accession number 
-    study = requests.post(urljoin(url, 'secure/search/query-data-source'), 
-        headers = header,
-        json = {'name': '',
-                'mrn': '',
-                'accNum': accession,
-                'dateRange': {'start':'','end':'','label':''},
-                'modality': '',
-                'sourceId': 1
-        }).json()['exams'][0]
+    # Search for study by accession number
+    study = requests.post(
+        urljoin(url, "secure/search/query-data-source"),
+        headers=header,
+        json={
+            "name": "",
+            "mrn": "",
+            "accNum": accession,
+            "dateRange": {"start": "", "end": "", "label": ""},
+            "modality": "",
+            "sourceId": 1,
+        },
+    ).json()["exams"][0]
 
     # Make a list of all included series
-    series = requests.post(urljoin(url, 'secure/search/series'),
-        headers = header,
-        json = study).json()
+    series = requests.post(
+        urljoin(url, "secure/search/series"), headers=header, json=study
+    ).json()
 
     # Filter series based on inclusion patterns, if provided
     if series_inclusion:
-        inclusion_list = [pattern.strip().lower()
-                          for pattern in series_inclusion.split(',')]
+        inclusion_list = [
+            pattern.strip().lower() for pattern in series_inclusion.split(",")
+        ]
         original_series_description = [serie["description"] for serie in series]
-        series = [serie for serie in series 
-                if (serie["description"] and any(pattern in serie["description"].lower()
-                for pattern in inclusion_list))]
+        series = [
+            serie
+            for serie in series
+            if (
+                serie["description"]
+                and any(
+                    pattern in serie["description"].lower()
+                    for pattern in inclusion_list
+                )
+            )
+        ]
         final_series_description = [serie["description"] for serie in series]
         print(f"Original series (n={len(original_series_description)}): ")
         print(original_series_description)
@@ -134,28 +137,28 @@ def download(url, cred_path, accession, output, project, profile, series_inclusi
         print(final_series_description)
 
     def has_started(download_info):
-        if 'downloadId' not in download_info:
-            raise RuntimeError(
-                f'download failed. Server Response: {download_info}')
+        if "downloadId" not in download_info:
+            raise RuntimeError(f"download failed. Server Response: {download_info}")
         check = requests.post(
-            urljoin(url, 'secure/search/download/check'),
-            headers = header,
-            json = {'downloadId': download_info['downloadId'],
-                'projectId': project
-            }).json()
-        return check['status'] in ['started', 'completed']
+            urljoin(url, "secure/search/download/check"),
+            headers=header,
+            json={"downloadId": download_info["downloadId"], "projectId": project},
+        ).json()
+        return check["status"] in ["started", "completed"]
 
     # Prepare download job
     download_info = requests.post(
-        urljoin(url, 'secure/search/download/start'),
-        headers = header,
-        json = {'decompress': False,
-                'name': 'Download.zip',
-                'profile': profile,
-                'projectId': project,
-                'series': series,
-                'study': study 
-        }).json()
+        urljoin(url, "secure/search/download/start"),
+        headers=header,
+        json={
+            "decompress": False,
+            "name": "Download.zip",
+            "profile": profile,
+            "projectId": project,
+            "series": series,
+            "study": study,
+        },
+    ).json()
 
     # Ensure that archive is ready for download
     while not has_started(download_info):
@@ -163,19 +166,23 @@ def download(url, cred_path, accession, output, project, profile, series_inclusi
 
     # Download archive
     download_stream = requests.post(
-        urljoin(url, 'secure/search/download/zip'),
-        headers = {'Upgrade-Insecure-Requests': '1'},
-        data = {
-            'params': json.dumps({
-                'downloadId': download_info['downloadId'],
-                'projectId': project,
-                'name': 'Download.zip'}),
-            'jwt': jwt
+        urljoin(url, "secure/search/download/zip"),
+        headers={"Upgrade-Insecure-Requests": "1"},
+        data={
+            "params": json.dumps(
+                {
+                    "downloadId": download_info["downloadId"],
+                    "projectId": project,
+                    "name": "Download.zip",
+                }
+            ),
+            "jwt": jwt,
         },
-         stream=True)
+        stream=True,
+    )
 
     # Save archive to disk
-    with open(output, 'wb') as fd:
+    with open(output, "wb") as fd:
         for chunk in download_stream.iter_content(chunk_size=8192):
             if chunk:
                 _ = fd.write(chunk)
@@ -183,9 +190,8 @@ def download(url, cred_path, accession, output, project, profile, series_inclusi
 
 def main(args):
     download(
-        args.url, args.cred_path,
-        args.acc, args.output,
-        args.project, args.profile)
+        args.url, args.cred_path, args.acc, args.output, args.project, args.profile
+    )
 
 
 def cli():
